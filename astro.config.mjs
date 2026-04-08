b023b7ec06838d320f379af79f283ac7c7267558
manpuc <guangxitengchuan@gmail.com>
2026-04-09 00:21:40 +0900

    feat: add sitemap integration and robots.txt configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://text.manpuc.me',
+  integrations: [react(), sitemap()],
   vite: {
     resolve: {
       alias: {
